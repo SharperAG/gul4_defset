50561345b530430ad58e9ac5af1b1fb07c73dc25
SharperJS <lonsharpjs@gmail.com>
2021-05-01 22:07:12 +0500

    new proj

---
+++ app/js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  console.log("hello");
+  console.log("im server");
+  console.log("im cliasdas");
+  console.log("clicked");
+  for (i = 0; i < 10; i++) {
+    console.log("i = ", i);
+  }
+});
